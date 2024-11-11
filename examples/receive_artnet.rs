dbf275c1ff395172ced7b8220a24e0e963de52e9
Timoteus Ruotsalainen <timoteus.ruotsalainen@otiru.fi>
2024-11-11 15:31:48 +0200

    remove unused warnings

--- examples/receive_artnet.rs
+++ examples/receive_artnet.rs
@@ -1,12 +1,12 @@
 use artnet_protocol::*;
-use std::net::{ToSocketAddrs, UdpSocket};
+use std::net::UdpSocket;
 
 fn main() {
     let socket = UdpSocket::bind(("0.0.0.0", 6454)).unwrap();
 
     loop {
         let mut buffer = [0u8; 1024];
-        let (length, addr) = socket.recv_from(&mut buffer).unwrap();
+        let (length, _addr) = socket.recv_from(&mut buffer).unwrap();
         let command = ArtCommand::from_buffer(&buffer[..length]).unwrap();
 
         println!("Received {:?}", command);
